a43f12b33e4d146f0e7e234fe5ba7620e76c2464
enigmatic6891-lgtm <enigmatic6891@gmail.com>
2026-09-26 10:36:04 +0600

    Update rules.mk

--- keyboards/handwired/z40qmk/rules.mk
+++ keyboards/handwired/z40qmk/rules.mk
@@ -13,14 +13,4 @@
 # LTO (Link-Time Optimization) and NKRO are set here because vial-qmk's
 # keyboard.json schema does not support these as features keys.
 
-# MCU и Архитектура для Raspberry Pi Pico
-MCU = RP2040
-BOOTLOADER = rp2040
-
-# Включение обязательных функций Vial и QMK
-VIAL_ENABLE = yes
 LTO_ENABLE = yes
-
-# Включение вашей ШИМ-подсветки
-BACKLIGHT_ENABLE = yes
-BACKLIGHT_DRIVER = pwm
